912d8158f40bdefeb846f8f17f2ad7972681ecda
guptaharsh847 <guptaharsh847@gmail.com>
2024-02-04 18:32:41 +0530

    admin page sidebar

---
+++ app/components/Admin/sidebar/AdminSidebar.tsx
@@ -0,0 +1,326 @@
+"use client";
+
+import { FC, useEffect, useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { Box, IconButton, Typography } from "@mui/material";
+import "react-pro-sidebar/dist/css/styles.css";
+import {
+  HomeOutlinedIcon,
+  ArrowForwardIosIcon,
+  ArrowBackIosIcon,
+  PeopleOutlinedIcon,
+  ReceiptOutlinedIcon,
+  BarChartoutlinedIcon,
+  MapoutlinedIcon,
+  GroupsIcon,
+  OndemandVideoIcon,
+  VideoCallIcon,
+  WebIcon,
+  QuizIcon,
+  WysiwygIcon,
+  ManageHistoryIcon,
+  SettingsIcon,
+  ExitToAppIcon,
+} from "./Icon";
+
+import avatarDefault from "../../../../public/assets/avatar.png";
+import { useSelector } from "react-redux";
+import Link from "next/link";
+import Image from "next/image";
+import { useTheme } from "next-themes";
+
+interface itemProps {
+  title: string;
+  to: string;
+  icon: JSX.Element;
+  selected: string;
+  setSelected: any;
+}
+
+const Item: FC<itemProps> = ({ title, to, icon, selected, setSelected }) => {
+  return (
+    <MenuItem
+      active={selected === title}
+      icon={icon}
+      onClick={() => setSelected(title)}
+    >
+      <Typography className="!text-[16px] !font-Poppins">{title}</Typography>
+      <Link href={to} />
+    </MenuItem>
+  );
+};
+
+const Sidebar = () => {
+  const { user } = useSelector((state: any) => state.auth);
+  const [logout, setlogout] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+  useEffect(() => setMounted(true), []);
+  if (!mounted) {
+    return null;
+  }
+  const logoutHandler = () => {
+    setlogout(true);
+  };
+  return (
+    <Box
+    sx={{
+    "& .pro-sidebar-inner": {
+     background: `${
+        theme === "dark" ? "#111C43 !important" : "#fff !important"}`,
+    
+    },
+    
+    "& .pro-icon-wrapper": {
+    
+    backgroundColor: "transparent !important",
+    
+    },
+    
+    "& .pro-inner-item:hover": {
+    
+    color: "#868dfb !important",
+    
+    },
+    
+    "& .pro-menu-item.active": {
+    
+    color: "#6870fa !important",
+    
+    },
+    
+    "& .pro-inner-item": {
+    
+    padding: "5px 35px 5px 20px !important",
+    
+    opacity: 1,
+    
+    },
+    
+    "& pro-menu-item": {
+    
+    color: `${theme != "dark" && "#000"}`,
+    
+    },
+    
+    }}
+    
+    className="!bg-white dark:bg-[#111C43]"
+    >
+    <ProSidebar
+    
+    collapsed={isCollapsed}
+    
+    style={{
+    
+    position: "fixed",
+    
+    top: 0,
+    
+    left: 0,
+    
+    height: "100vh",
+    width: isCollapsed ? "0%" : "16%",
+    }}
+    >
+        <Menu iconShape="square">
+
+{/* LOGO AND MENU ICON */}
+
+<MenuItem
+
+onClick={() => setIsCollapsed(!isCollapsed)}
+
+icon={isCollapsed ? <ArrowForwardIosIcon />: undefined}
+
+style={{
+
+margin: "10px 0 20px 0",
+
+}}
+>
+
+{!isCollapsed && (
+<Box
+display="flex"
+justifyContent="space-between"
+alignItems="center"
+ml="15px">
+<Link href="/">
+<h3 className="text-[25px] font-Poppins uppercase dark:text-white ☐ text-black">
+Elearning
+</h3>
+</Link>
+
+<IconButton onClick={() => setIsCollapsed (!isCollapsed)} className="inline-block">
+<ArrowBackIosIcon className="text-black dark:text-[#ffffffc1]" />
+</IconButton>
+</Box>
+)}
+</MenuItem>
+
+
+{!isCollapsed && (
+
+<Box mb="25px">
+
+<Box display="flex" justifyContent="center" alignItems="center">
+<Image
+alt="profile-user"
+width={100}
+height={100}
+src={user.avatar ? user.avatar.url : avatarDefault}
+style={{ cursor: "pointer", borderRadius: "50%", border: "3px solid #5b6fe6", }}
+/>
+</Box>
+<Box textAlign="center">
+    <Typography
+    variant="h4"
+    color="#000"
+    fontWeight="bold"
+    sx={{ m: "10px 0 0 0" }}
+    >
+    {user?.name}
+    </Typography>
+    <Typography variant="h6" 
+    className="!text-[20px] text-black dark:text-[#ffffffc1] !font-Poppins capitalize" sx={{ m: "10px 0 0 0" }}>
+   - {user.role}
+    </Typography>
+    </Box>
+</Box>
+)}
+
+{/* MENU ITEMS */}
+<Box paddingLeft={isCollapsed ? undefined : "10%"}>
+    <Item
+    title="Dashboard"
+    to="/admin"
+    icon={<HomeOutlinedIcon />}
+    selected={selected}
+    setSelected={setSelected}
+    />
+    <Typography variant="h5" 
+    className="!text-[18px] text-black dark:text-[#ffffffc1] !font-Poppins !font-[400] capitalize" sx={{ m: "15px 0 5px 25px" }}>
+   {!isCollapsed && "Data"}
+    </Typography>
+    <Item
+    title="Users"
+    to="/admin/users"
+    icon={<GroupsIcon />}
+    selected={selected}
+    setSelected={setSelected}
+    />
+    <Item
+    title="Invoices"
+    to="/admin/invoices"
+    icon={<ReceiptOutlinedIcon />}
+    selected={selected}
+    setSelected={setSelected}
+    />
+    <Typography variant="h5" 
+    className="!text-[18px] text-black dark:text-[#ffffffc1] !font-Poppins !font-[400] capitalize" sx={{ m: "15px 0 5px 25px" }}>
+   {!isCollapsed && "Content"}
+    </Typography>
+    <Item
+    title="Create Course"
+    to="/admin/create-course"
+    icon={<VideoCallIcon />}
+    selected={selected}
+    setSelected={setSelected}
+    />
+    <Item
+    title="Live Classes"
+    to="/admin/live-class"
+    icon={<OndemandVideoIcon />}
+    selected={selected}
+    setSelected={setSelected}
+    />
+  <Typography variant="h5" 
+    className="!text-[18px] text-black dark:text-[#ffffffc1] !font-Poppins !font-[400] capitalize" sx={{ m: "15px 0 5px 25px" }}>
+   {!isCollapsed && "Customization"}
+    </Typography>
+    <Item
+    title="hero"
+    to="/admin/hero"
+    icon={<WebIcon />}
+    selected={selected}
+    setSelected={setSelected}
+    />
+    <Item
+    title="FAQ"
+    to="/admin/faq"
+    icon={<QuizIcon />}
+    selected={selected}
+    setSelected={setSelected}
+    />
+    <Item
+    title="Categories"
+    to="/admin/categories"
+    icon={<WysiwygIcon />}
+    selected={selected}
+    setSelected={setSelected}
+    />
+    <Typography variant="h5" 
+    className="!text-[18px] text-black dark:text-[#ffffffc1] !font-Poppins !font-[400] capitalize" sx={{ m: "15px 0 5px 25px" }}>
+   {!isCollapsed && "Controllers"}
+    </Typography>
+    <Item
+    title="Manage Team"
+    to="/admin/manage-team"
+    icon={<PeopleOutlinedIcon />}
+    selected={selected}
+    setSelected={setSelected}
+    />
+    <Typography variant="h5" 
+    className="!text-[18px] text-black dark:text-[#ffffffc1] !font-Poppins !font-[400] capitalize" sx={{ m: "15px 0 5px 25px" }}>
+   {!isCollapsed && "Analytics"}
+    </Typography>
+    <Item
+    title="Course Analytics"
+    to="/admin/course-analytics"
+    icon={<BarChartoutlinedIcon />}
+    selected={selected}
+    setSelected={setSelected}
+    />
+    <Item
+    title="Order Analytics"
+    to="/admin/order-analytics"
+    icon={<MapoutlinedIcon />}
+    selected={selected}
+    setSelected={setSelected}
+    />
+    <Item
+    title="User Analytics"
+    to="/admin/user-analytics"
+    icon={<ManageHistoryIcon />}
+    selected={selected}
+    setSelected={setSelected}
+    />
+     <Typography variant="h5" 
+    className="!text-[18px] text-black dark:text-[#ffffffc1] !font-Poppins !font-[400] capitalize" sx={{ m: "15px 0 5px 25px" }}>
+   {!isCollapsed && "Extras"}
+    </Typography>
+    <Item
+    title="Settings"
+    to="/admin/settings"
+    icon={<SettingsIcon />}
+    selected={selected}
+    setSelected={setSelected}
+    />
+    <Item
+    title="Log Out"
+    to="/admin/logout"
+    icon={<ExitToAppIcon />}
+    selected={selected}
+    setSelected={setSelected}
+    />
+</Box>
+
+</Menu>
+</ProSidebar>
+</Box>
+);
+};
+export default Sidebar;
--- app/components/Profile/SideBarProfile.tsx
+++ app/components/Profile/SideBarProfile.tsx
@@ -5,6 +5,7 @@ import { RiAdminFill, RiLockPasswordLine } from "react-icons/ri";
 import { SiCoursera } from "react-icons/si";
 import { AiOutlineLogout } from "react-icons/ai";
 import { MdAdminPanelSettings } from "react-icons/md";
+import Link from "next/link";
 
 type Props = {
   user: any;
@@ -30,54 +31,72 @@ const SideBarProfile: FC<Props> = ({
         onClick={() => setActive(1)}
       >
         <Image
-          src={user.avatar || avatar ? user.avatar.url || avatar : avatarDefault}
+          src={
+            user.avatar || avatar ? user.avatar.url || avatar : avatarDefault
+          }
           alt=""
           width={20}
           height={20}
           className="w-[20px] h-[20px] 800px:w-[30px] 800px:h-[30px] cursor-pointer rounded-full "
         />
-        <h5 className="pt-1 px-2 dark:text-white text-[20px] text-black font-[600] 800px:block hidden font-Poppins">My Account</h5>
+        <h5 className="pt-1 px-2 dark:text-white text-[20px] text-black font-[600] 800px:block hidden font-Poppins">
+          My Account
+        </h5>
       </div>
       <div
         className={`w-full   flex items-center px-3 py-4 cursor-pointer  ${
-            active === 2 ? "dark:bg-slate-800 bg-[#b6c1c9]" : "bg-transparent"
-          }`}
-          onClick={() => setActive(2)}
-        >
-            <RiLockPasswordLine size={20} className="dark:text-white text-black"/>
-            <h5 className=" px-2 dark:text-white text-[20px] text-black font-[400] 800px:block hidden font-Poppins">Change Password</h5>
-      
+          active === 2 ? "dark:bg-slate-800 bg-[#b6c1c9]" : "bg-transparent"
+        }`}
+        onClick={() => setActive(2)}
+      >
+        <RiLockPasswordLine size={20} className="dark:text-white text-black" />
+        <h5 className=" px-2 dark:text-white text-[20px] text-black font-[400] 800px:block hidden font-Poppins">
+          Change Password
+        </h5>
       </div>
-      <div
-        className={`w-full   flex items-center px-3 py-4 cursor-pointer  ${
+      {user.role === "admin" && (
+        <Link
+          className={`w-full   flex items-center px-3 py-4 cursor-pointer  ${
             active === 5 ? "dark:bg-slate-800 bg-[#b6c1c9]" : "bg-transparent"
           }`}
-          onClick={() => setActive(5)}
+          href={"/admin"}
+        
         >
-            <RiAdminFill  size={20} className="dark:text-white  text-black" />
-            <h5 className=" px-2 dark:text-white text-[20px] text-black font-[400] 800px:block hidden font-Poppins">Admin Dashboard</h5>
-      
-      </div>
-      <div
+          <RiAdminFill size={20} className="dark:text-white  text-black" />
+          <h5 className=" px-2 dark:text-white text-[20px] text-black font-[400] 800px:block hidden font-Poppins">
+            Admin Dashboard
+          </h5>
+        </Link>
+      )}
+     {
+      user.role ==="user" && (
+        <div
         className={`w-full   flex items-center px-3 py-4 cursor-pointer  ${
-            active === 3 ? "dark:bg-slate-800 bg-[#b6c1c9]" : "bg-transparent"
-          }`}
-          onClick={() => setActive(3)}
-        >
-            <SiCoursera size={20} className="dark:text-white text-black"/>
-            <h5 className=" px-2 dark:text-white text-[20px] text-black font-[400] 800px:block hidden font-Poppins">Enrolled Courses</h5>
-      
+          active === 3 ? "dark:bg-slate-800 bg-[#b6c1c9]" : "bg-transparent"
+        }`}
+        onClick={() => setActive(3)}
+      >
+        <SiCoursera size={20} className="dark:text-white text-black" />
+        <h5 className=" px-2 dark:text-white text-[20px] text-black font-[400] 800px:block hidden font-Poppins">
+          Enrolled Courses
+        </h5>
       </div>
+      )
+     }
       <div
         className={`w-full  flex items-center px-3 py-4 cursor-pointer  ${
-            active === 4 ? "dark:bg-slate-800 bg-[rgb(182,193,201)]" : "bg-transparent"
-          }`}
-          onClick={() => {
-            logoutHandler()}}
-        >
-            <AiOutlineLogout size={20} className="dark:text-white text-black"/>
-            <h5 className=" px-2 dark:text-white text-[20px] text-black font-[400] 800px:block hidden font-Poppins">Log Out</h5>
-      
+          active === 4
+            ? "dark:bg-slate-800 bg-[rgb(182,193,201)]"
+            : "bg-transparent"
+        }`}
+        onClick={() => {
+          logoutHandler();
+        }}
+      >
+        <AiOutlineLogout size={20} className="dark:text-white text-black" />
+        <h5 className=" px-2 dark:text-white text-[20px] text-black font-[400] 800px:block hidden font-Poppins">
+          Log Out
+        </h5>
       </div>
     </div>
   );
